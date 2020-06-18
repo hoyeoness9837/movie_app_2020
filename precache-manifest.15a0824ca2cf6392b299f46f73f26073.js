fca2edc21e584f12517c05d439b191b45810a483
Hoyeon Kim <kim.hoyeon93@gmail.com>
2020-06-18 01:39:53 -0700

    Updates

---
+++ precache-manifest.15a0824ca2cf6392b299f46f73f26073.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d7b4a9da6550f8198913e08315000fc",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "d3099c40b088ce5a6079",
+    "url": "/movie_app_2020/static/css/main.f9a8a042.chunk.css"
+  },
+  {
+    "revision": "79907e5e853fc1fa7794",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.0285f39f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3099c40b088ce5a6079",
+    "url": "/movie_app_2020/static/js/main.bf333ed1.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
